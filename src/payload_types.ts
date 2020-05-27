e071bd6e79d7c70e880f412fedd80a8dd0031dca
Brandon Johnson <zebrahome10@gmail.com>
2020-05-26 23:34:00 -0400

    Export payloadtypes, re-order for readability

--- src/payload_types.ts
+++ src/payload_types.ts
@@ -1,3 +1,6 @@
+//FOLLOW EVENT DATA TYPE(S)
+import {WebhookType} from "./webhooks";
+
 type FollowEvent = {
     from_id: string,
     from_name: string,
@@ -6,6 +9,7 @@ type FollowEvent = {
     followed_at: Date
 };
 
+//STREAM CHANGED EVENT DATA TYPE(S)
 type StreamChangedEvent = {
     id: string,
     user_id: string,
@@ -20,6 +24,7 @@ type StreamChangedEvent = {
     thumbnail_url: string
 };
 
+//USER CHANGED EVENT DATA TYPE(S)
 type UserChangedEvent = {
     id: string,
     login: string,
@@ -32,21 +37,7 @@ type UserChangedEvent = {
     view_count: number,
 };
 
-type CostData = {
-    amount: number,
-    type: string, // Always "Bits",
-}
-
-type ProductData = {
-    domain: string,
-    broadcast: boolean,
-    expiration: string, // always empty (only unexpired products can be purchased)
-    sku: string,
-    cost: CostData,
-    displayName: string,
-    inDevelopment: boolean
-}
-
+//EXTENSION TRANSACTION EVENT DATA TYPE(S)
 type ExtensionTransactionCreatedEvent = {
     id: string,
     timestamp: Date,
@@ -58,18 +49,28 @@ type ExtensionTransactionCreatedEvent = {
     product_data: ProductData,
 };
 
+type ProductData = {
+    domain: string,
+    broadcast: boolean,
+    expiration: string, // always empty (only unexpired products can be purchased)
+    sku: string,
+    cost: CostData,
+    displayName: string,
+    inDevelopment: boolean
+}
+
+type CostData = {
+    amount: number,
+    type: string, // Always "Bits",
+}
+
+
+//MODERATOR EVENT DATA TYPES
 enum ModeratorEventType {
     MODERATOR_ADD = "moderation.moderator.add",
     MODERATOR_REMOVE = "moderation.moderator.remove",
     BAN_USER = "moderation.user.ban",
     UNBAN_USER = "moderation.user.unban"
-}
-
-type ModeratorEventData = {
-    broadcaster_id: string,
-    broadcaster_name: string,
-    user_id: string,
-    user_name: string
 }
 
 type ModeratorChangeEvent = {
@@ -80,6 +81,15 @@ type ModeratorChangeEvent = {
     event_data: ModeratorEventData// TODO: Check if this is only mod add and remove - not ban & unban
 };
 
+type ModeratorEventData = {
+    broadcaster_id: string,
+    broadcaster_name: string,
+    user_id: string,
+    user_name: string
+}
+
+
+//BAN EVENT DATA TYPES
 type ChannelBanChangeEvent = {
     id: string, // Docs says that this is the user id of the moderator. There is no way I buy that for a second. This is the unique event ID.
     event_type: ModeratorEventType,
@@ -88,11 +98,20 @@ type ChannelBanChangeEvent = {
     event_data: ModeratorEventData // TODO: Check if this is only ban & unban - not mod add and remove
 };
 
+//SUBSCRIPTION EVENT DATA TY{ES
 enum SubscriptionEventType {
     SUBSCRIBE = "subscriptions.subscribe",
     NOTIFICATION = "subscriptions.notification",
     UNSUBSCRIBE = "subscriptions.unsubscribe"
 }
+
+type SubscriptionEvent = {
+    id: string,
+    event_type: SubscriptionEventType,
+    event_timestamp: Date,
+    version: string,
+    event_data: SubscriptionEventData
+};
 
 type SubscriptionEventData = {
     broadcaster_id: string,
@@ -105,20 +124,35 @@ type SubscriptionEventData = {
     message?: string
 }
 
-type SubscriptionEvent = {
-    id: string,
-    event_type: SubscriptionEventType,
-    event_timestamp: Date,
-    version: string,
-    event_data: SubscriptionEventData
-};
-
-type WebhookPayload = {
-    data: FollowEvent | StreamChangedEvent | UserChangedEvent | ExtensionTransactionCreatedEvent | ModeratorChangeEvent | ChannelBanChangeEvent | SubscriptionEvent;
-};
+//BASE WEBHOOK EVENT OBJECT
+// On a human note, conditional types are so f***ing cool! Try doing THIS in Java!
+type WebhookPayload<T extends WebhookType> = {
+    type: T,
+    data: T extends WebhookType.UserFollows ? FollowEvent :
+          T extends WebhookType.StreamChanged ? StreamChangedEvent :
+          T extends WebhookType.UserChanged ? UserChangedEvent :
+          T extends WebhookType.ExtensionTransactionCreated ? ExtensionTransactionCreatedEvent :
+          T extends WebhookType.ModeratorChange ? ModeratorChangeEvent :
+          T extends WebhookType.ChannelBanChange ? ChannelBanChangeEvent :
+          T extends WebhookType.Subscription ? SubscriptionEvent :
+          undefined
+}
 
 //TODO write function to coerce "any" type to the above types.
 
 export {
+    FollowEvent,
+    StreamChangedEvent,
+    UserChangedEvent,
+    ExtensionTransactionCreatedEvent,
+    ProductData,
+    CostData,
+    ModeratorEventType,
+    ModeratorChangeEvent,
+    ModeratorEventData,
+    ChannelBanChangeEvent,
+    SubscriptionEventType,
+    SubscriptionEvent,
+    SubscriptionEventData,
     WebhookPayload
 }
